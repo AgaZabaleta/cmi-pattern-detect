453b6efc7174bf5bf8a429c9d5752dc1ffb91cc3
Aga <aga.zabaleta.mabolo@gmail.com>
2019-04-18 11:30:22 +0200

    Change arguments

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-import sys
+import sys, getopt
 from os.path import basename
 import pandas as pd
 import file_load
 import clusters
 
-def main():
-    if len(sys.argv) != 7:
-        sys.exit()
-
-    start = sys.argv[1]
-    end = sys.argv[2]
-    time_interval = sys.argv[3]
-    eps = float(sys.argv[4])
-    mint = int(sys.argv[5])
-    file_path = sys.argv[6]
+def getmove(argv):
+    start = argv[0]
+    end = argv[1]
+    time_interval = argv[2]
+    eps = float(argv[3])
+    mint = int(argv[4])
+    file_path = argv[5]
     file_name = basename(file_path)
-
     df = file_load.import_file(file_path)
-    
 
     if start.upper().isupper():
         start = df['timestamp'].min()
@@ -32,14 +27,28 @@ def main():
 
     date_range = pd.date_range(start=start, end=end, freq=time_interval)
 
+    patterns_json = clusters.get_patterns(df, eps, mint, date_range)
+
+    file_load.write_json(patterns_json, name="patterns_"+file_name)
+
+def raws(file_path):
+    file_name = basename(file_path)
+    df = file_load.import_file(file_path)
+
     raws_json = file_load.get_raw_data(df)
     raws_json["filename"] = file_name
 
     file_load.write_json(raws_json, name="raws_"+file_name)
 
-    patterns_json = clusters.get_patterns(df, eps, mint, date_range)
+def main(argv):
+    if len(argv) < 2:
+        sys.exit("Missing arguments: please check documentation on how to use this script")
 
-    file_load.write_json(patterns_json, name="patterns_"+file_name)
+    if argv[0] == "getmove":
+        if len(argv[1:]) == 6:
+            getmove(argv[1:])
+    elif argv[0] == "raws":
+        raws(argv[1])
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
